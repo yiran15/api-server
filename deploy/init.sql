b23c9f54b52e3a6dd00e04c4744ed2f2ea4c5bc6
清泉流响 <qinquanliuxiang@qq.com>
2025-07-28 23:54:44 +0800

    fix: cicd

--- deploy/init.sql
+++ deploy/init.sql
@@ -24,7 +24,6 @@ CREATE TABLE `roles` (
   `description` VARCHAR(255)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 CREATE INDEX `idx_roles_deleted_at` ON `roles` (`deleted_at`);
-INSERT INTO `roles` (`id`, `created_at`, `updated_at`, `deleted_at`, `name`, `description`) VALUES (1, '2025-07-19 16:15:22', '2025-07-19 16:15:22', NULL, 'admin', 'admin');
 
 -- 用户角色多对多关联表
 CREATE TABLE `user_roles` (
@@ -33,7 +32,7 @@ CREATE TABLE `user_roles` (
   PRIMARY KEY (`user_id`, `role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---- 接口信息表
+-- 接口信息表
 CREATE TABLE `apis` (
   `id` BIGINT PRIMARY KEY AUTO_INCREMENT,
   `created_at` DATETIME NOT NULL,
@@ -47,13 +46,14 @@ CREATE TABLE `apis` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 CREATE INDEX `idx_apis_deleted_at` ON `apis` (`deleted_at`);
 
---- 角色API列表多对多关联表
+-- 角色API列表多对多关联表
 CREATE TABLE `role_apis` (
   `role_id` BIGINT UNSIGNED NOT NULL,
   `api_id` BIGINT UNSIGNED NOT NULL,
   PRIMARY KEY (`role_id`, `api_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- casbin 规则表
 CREATE TABLE `casbin_rule`
 (
     id    bigint unsigned primary key auto_increment,
@@ -67,4 +67,3 @@ CREATE TABLE `casbin_rule`
     constraint idx_casbin_rule
         unique (ptype, v0, v1, v2, v3, v4, v5)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-INSERT INTO `casbin_rule` (`id`, `ptype`, `v0`, `v1`, `v2`, `v3`, `v4`, `v5`) VALUES (1, 'p', 'admin', '*', '*', NULL, NULL, NULL);
