45f62ea01d68e67f4e33d92a171f5877d2175a6d
清泉流响 <qinquanliuxiang@qq.com>
2025-08-05 18:57:59 +0800

    update: 更新用户修改角色逻辑

--- base/helper/role.go
+++ base/helper/role.go
@@ -1,19 +1,18 @@
 package helper
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/yiran15/api-server/model"
 )
 
 func ValidateRoleIds(reqRoleIds []int64, roles []*model.Role, total int64) error {
-	if len(reqRoleIds) == 0 {
-		return errors.New("role ids is empty")
-	}
-	if len(roles) == 0 {
-		return errors.New("roles is empty")
-	}
+	// if len(reqRoleIds) == 0 {
+	// 	return errors.New("role ids is empty")
+	// }
+	// if len(roles) == 0 {
+	// 	return errors.New("roles is empty")
+	// }
 	if len(reqRoleIds) == int(total) {
 		return nil
 	}
