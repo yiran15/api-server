e9c9e0fec2b94dccf9fd8ffdfa5b2d8288fe029c
清泉流响 <qinquanliuxiang@qq.com>
2025-08-13 12:13:40 +0800

    fix: **authz 逻辑修改**
    增加用户空角色逻辑，防止重复查询数据量，造成缓存穿透。

---
+++ test/cache/cache_test.go
@@ -0,0 +1,51 @@
+package cache_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/yiran15/api-server/base/conf"
+	"github.com/yiran15/api-server/base/data"
+	"github.com/yiran15/api-server/base/log"
+	"github.com/yiran15/api-server/store"
+	"go.uber.org/zap"
+)
+
+var (
+	cacheStore store.CacheStorer
+	closeup    func()
+)
+
+func init() {
+	var (
+		redisClient *redis.Client
+		err         error
+	)
+	conf.LoadConfig("../../config.yaml")
+	redisClient, err = data.NewRDB()
+	if err != nil {
+		panic(err)
+	}
+	log.NewLogger()
+	cacheStore, closeup, err = store.NewCacheStore(redisClient)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestCacheStore(t *testing.T) {
+	defer closeup()
+	var (
+		roles []string
+		err   error
+	)
+	roleNames := []any{"test"}
+	if err := cacheStore.SetSet(context.Background(), store.RoleType, "test", roleNames, nil); err != nil {
+		t.Fatal(err)
+	}
+	if roles, err = cacheStore.GetSet(context.Background(), store.RoleType, "test"); err != nil {
+		t.Fatal(err)
+	}
+	zap.L().Info("roles", zap.Any("roles", roles))
+}
