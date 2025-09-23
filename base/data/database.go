2cdda8eedbb051fe914ab4562afc915817963fdd
清泉流响 <qinquanliuxiang@qq.com>
2025-09-23 11:27:44 +0800

    ft: 部署文件增加 nginx 配置

--- base/data/database.go
+++ base/data/database.go
@@ -2,7 +2,6 @@ package data
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/spf13/viper"
 	"github.com/yiran15/api-server/base/conf"
@@ -12,37 +11,37 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-type zapWriter struct {
-	log *zap.Logger
-}
+// type zapWriter struct {
+// 	log *zap.Logger
+// }
 
-func (w zapWriter) Printf(_ string, args ...interface{}) {
-	// gorm: l.Printf(l.traceErrStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, rows, sql)
-	if len(args) == 5 {
-		w.log.Error("gorm log", zap.String("err", args[1].(error).Error()), zap.Float64("elapsed", args[2].(float64)), zap.String("rows", "-"), zap.String("sql", args[4].(string)))
-		return
-	}
-	switch args[2].(type) {
-	case int64:
-		w.log.Info("gorm log", zap.Float64("elapsed", args[1].(float64)), zap.Int64("rows", args[2].(int64)), zap.String("sql", args[3].(string)))
-	case string:
-		w.log.Info("gorm log", zap.Float64("elapsed", args[1].(float64)), zap.String("rows", args[2].(string)), zap.String("sql", args[3].(string)))
-	default:
-		w.log.Info("gorm log", zap.Float64("elapsed", args[1].(float64)), zap.String("rows", "-"), zap.String("sql", args[3].(string)))
-	}
-}
+// func (w zapWriter) Printf(_ string, args ...interface{}) {
+// 	// gorm: l.Printf(l.traceErrStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, rows, sql)
+// 	if len(args) == 5 {
+// 		w.log.Error("gorm log", zap.String("err", args[1].(error).Error()), zap.Float64("elapsed", args[2].(float64)), zap.String("rows", "-"), zap.String("sql", args[4].(string)))
+// 		return
+// 	}
+// 	switch args[2].(type) {
+// 	case int64:
+// 		w.log.Info("gorm log", zap.Float64("elapsed", args[1].(float64)), zap.Int64("rows", args[2].(int64)), zap.String("sql", args[3].(string)))
+// 	case string:
+// 		w.log.Info("gorm log", zap.Float64("elapsed", args[1].(float64)), zap.String("rows", args[2].(string)), zap.String("sql", args[3].(string)))
+// 	default:
+// 		w.log.Info("gorm log", zap.Float64("elapsed", args[1].(float64)), zap.String("rows", "-"), zap.String("sql", args[3].(string)))
+// 	}
+// }
 
 // newGormLogger create a new gorm logger
-func newGormLogger(z *zap.Logger) logger.Interface {
-	return logger.New(
-		zapWriter{log: z}, // 使用 zap writer
-		logger.Config{
-			SlowThreshold: time.Second, // 慢查询阈值
-			LogLevel:      logger.Info, // 级别
-			Colorful:      true,
-		},
-	)
-}
+// func newGormLogger(z *zap.Logger) logger.Interface {
+// 	return logger.New(
+// 		zapWriter{log: z}, // 使用 zap writer
+// 		logger.Config{
+// 			SlowThreshold: time.Second, // 慢查询阈值
+// 			LogLevel:      logger.Info, // 级别
+// 			Colorful:      true,
+// 		},
+// 	)
+// }
 
 func NewDB() (*gorm.DB, func(), error) {
 	dsn, err := conf.GetMysqlDsn()
@@ -52,7 +51,8 @@ func NewDB() (*gorm.DB, func(), error) {
 	var dbLogger logger.Interface
 	// 开启mysql日志
 	if viper.GetBool("mysql.debug") || conf.GetLogLevel() == "debug" {
-		dbLogger = newGormLogger(zap.L())
+		// dbLogger = newGormLogger(zap.L())
+		dbLogger = logger.Default.LogMode(logger.Info)
 		zap.S().Info("enable debug mode on the database")
 	}
 
